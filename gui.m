377e988b8d8b769e46825379216093d3d3a64ef6
Oguz Kagan EREN <oguz.eren@ceng.deu.edu.tr>
2019-12-07 21:21:23 +0300

    first commit

---
+++ gui.m
@@ -0,0 +1,5 @@
+% copyfile(fullfile(docroot, 'techdoc','creating_guis',...
+%    'examples','simple_gui*.*')),fileattrib('simple_gui*.*', '+w');
+% DO NOT RUN IT AGAIN!
+guide simple_gui.fig;
+edit simple_gui.m
